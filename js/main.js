4cc40c457f11dad27a1ecf7d05b2136fbf896864
Chris <sunghew@gmail.com>
2016-09-17 14:42:32 -0700

    js submit might work

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
-   
+
     var submit_btn = $('#submit');
-       
+
     submit_btn.click(function(){
-        
+
         var age = $('#age').val();
         var sex = $('#sex').val();
         var mar = $('#mar').val();
         var ind = $('#ind').val();
         var loc = $('#loc').val();
-        
-        $('#info').html('hello');
-        
+
+        // use $.get to send http request to url?age=&sex=
+        $.get('https://8e22f0d3.ngrok.io/salaries', { "age": age, "sex": sex, "mar": mar, "ind": ind, "loc": loc, "x": 5 }, function(res){
+          $('#info').html(res);
+        })
+
+
     })
-   
+
 })
