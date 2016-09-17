994a364c7a7f22ba453d483de7fdcda2b9109fa1
Chris <sunghew@gmail.com>
2016-09-17 15:14:49 -0700

    js

--- js/main.js
+++ js/main.js
@@ -10,9 +10,15 @@ $(document).ready(function(){
         var loc = $('#loc').val();
 
         $.get('https://8e22f0d3.ngrok.io/salaries', { "age": age, "sex": sex, "mar": mar, "ind": ind, "loc": loc, "x": 5 }, function(res){
-          console.log('hi');
           console.log(res);
           $('#info').html(res);
+          $('#salary').html(res['salary'])
+          $('#con_hardship').html(res['con_hardship'])
+          $('#con_price').html(res['con_price'])
+          $('#house_hardship').html(res['house_hardship'])
+          $('#sfr_price').html(res['sfr_price'])
+          $('#time_to_con').html(res['time_to_con'])
+          $('#time_to_house').html(res['time_to_house'])
         })
 
     })
